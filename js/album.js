88afadbe52c4bd62caa9f94be4818351af031ca6
jianyuqi123 <115988158+jianyuqi123@users.noreply.github.com>
2026-05-25 01:53:16 +0800

    Add files via upload

---
+++ js/album.js
@@ -0,0 +1,48 @@
+const album = [
+  {
+    title: "自然历史",
+
+    cover: "picture/ziranlishi.jpg",
+
+    type: "album",
+
+    time: "2026.8.21",
+
+    info: "9",
+
+    note: "这是一个自然历史的专辑简介",
+
+    songs: [
+      "孤季",
+      "鲁镇",
+      "听说",
+      "那些美好",
+      "告白与告别",
+      "我愿陪你颠沛流离",
+      "圣诞孤单",
+      "再抄写一遍",
+      "遥远和船"
+    ]
+  },
+
+  {
+    title: "午夜短篇",
+
+    cover: "picture/zhanweifu.png",
+
+    type: "ep",
+
+    time: "2026.10.1",
+
+    info: "5",
+
+    note: "实验性EP",
+
+    songs: [
+      "孤季",
+      "鲁镇"
+    ]
+  }
+];
+
+window.album = album;
